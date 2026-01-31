e78b83f4409ffa2ba4b87bc5ad4bb3efd0ce639b
Adarrot <adam.darrot@etu.imt-nord-europe.fr>
2026-01-31 21:37:22 +0100

    blablabla commit

--- src/main/java/imt/fisa/player/persistence/dto/ProfileEntity.java
+++ src/main/java/imt/fisa/player/persistence/dto/ProfileEntity.java
@@ -1,4 +1,15 @@
 package imt.fisa.player.persistence.dto;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection = "profile")
 public class ProfileEntity {
+    @MongoId
+    private String identifiant;
+
+    private int level;
+
+    private int experience;
+
 }
