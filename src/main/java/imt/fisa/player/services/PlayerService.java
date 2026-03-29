dd402fcd56df92b16fe857e319484f20aa5dabd2
darrotaadam <adam.darrot@etu.imt-nord-europe.fr>
2026-03-29 20:17:56 +0200

    add: methods to PlayerService

--- src/main/java/imt/fisa/player/services/PlayerService.java
+++ src/main/java/imt/fisa/player/services/PlayerService.java
@@ -1,5 +1,7 @@
 package imt.fisa.player.services;
 
+import imt.fisa.player.exceptions.InternalServerErrorException;
+import imt.fisa.player.persistence.dto.Monstres;
 import imt.fisa.player.persistence.dto.ProfileEntity;
 import imt.fisa.player.persistence.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +20,84 @@ public class PlayerService {
                 );
     }
 
-
-
-    public int getMaxExperienceForLevel(int level){
-        return (int) (50 * ( (double) level * 1.1)) ;
+    public void ajoutMonstre(ProfileEntity player, String idMonstre){
+        player.getMonstres().add(new Monstres(idMonstre));
+        profileRepository.save(player);
     }
 
+    public void supprimeMonstre(ProfileEntity player, String idMonstre){
+        player.getMonstres().removeIf(monstre -> monstre.getId().equals(idMonstre));
+        profileRepository.save(player);
+    }
+
+
+
+
+    public void gainExperience(ProfileEntity player, int experienceGained){
+
+        if(player.getLevel()<1){
+            throw new InternalServerErrorException("Le niveau du joueur ne peut pas être inférieur à 1");
+        }
+        int initialExperience = player.getExperience();
+        int remainingExperienceToGain = experienceGained;
+        int gainedLevels = 0;
+        int xpNeededForLevelUp =  (int) (50 * Math.pow(1.1, player.getLevel()-1)) - initialExperience;
+
+        // en cas de dépassement de l'xp necessaire ==> on levelip tant qu'on peut
+        while (remainingExperienceToGain >= xpNeededForLevelUp && player.getLevel()+gainedLevels<50){
+            initialExperience = 0;
+            remainingExperienceToGain -= xpNeededForLevelUp;
+            gainedLevels++;
+
+            xpNeededForLevelUp =  (int) (50 *  Math.pow(1.1, player.getLevel() + gainedLevels -1) );
+        }
+
+        player.setLevel( player.getLevel() + gainedLevels);
+
+        player.setExperience( initialExperience + remainingExperienceToGain );
+        profileRepository.save(player);
+    }
+
+
+
+
+
+
+
+
+    public void gainExperience(ProfileEntity player, int experienceGained,boolean debugMode){
+
+        if(player.getLevel()<1){
+            throw new InternalServerErrorException("Le niveau du joueur ne peut pas être inférieur à 1");
+        }
+        int initialExperience = player.getExperience();
+        int remainingExperienceToGain = experienceGained;
+        int gainedLevels = 0;
+        int xpNeededForLevelUp =  (int) (50 * Math.pow(1.1, player.getLevel()-1)) - initialExperience;
+
+        System.out.println("Player" + player.getIdentifiant() + " is at level " + player.getLevel());
+        System.out.println("Player " + player.getIdentifiant() + " has " + initialExperience);
+        System.out.println("Player " + player.getIdentifiant() + " will gain " + experienceGained);
+        System.out.println("Player " + player.getIdentifiant() + " needs " + xpNeededForLevelUp + " to level up");
+        System.out.println("Player " + player.getIdentifiant() + " has " + remainingExperienceToGain + " experience left to gain");
+        // en cas de dépassement de l'xp necessaire ==> on levelip tant qu'on peut
+        while (remainingExperienceToGain >= xpNeededForLevelUp && player.getLevel()+gainedLevels<50){
+            initialExperience = 0;
+            System.out.println("Player " + player.getIdentifiant() + " is at level " + (player.getLevel() + gainedLevels) + " and needs " + xpNeededForLevelUp + " to level up");
+            remainingExperienceToGain -= xpNeededForLevelUp;
+            gainedLevels++;
+            System.out.println("Player " + player.getIdentifiant() + " leveled up to " + (player.getLevel() + gainedLevels) + " and has " + remainingExperienceToGain + " experience left to gain");
+
+            xpNeededForLevelUp =  (int) (50 *  Math.pow(1.1, player.getLevel() + gainedLevels -1) );
+        }
+
+        player.setLevel( player.getLevel() + gainedLevels);
+
+        System.out.println("Player " + player.getIdentifiant() + " is now at level " + player.getLevel());
+        System.out.println("Player " + player.getIdentifiant() + " has " + remainingExperienceToGain + " experience left");
+        player.setExperience( initialExperience + remainingExperienceToGain );
+        //profileRepository.save(player);
+    }
 
 
 }
