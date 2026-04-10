55d860a74fdb6958432ce34737f4bfabeeb04f0b
darrotaadam <adam.darrot@etu.imt-nord-europe.fr>
2026-04-10 14:12:36 +0200

    add: monsterEntity for client rendering

---
+++ src/main/java/imt/fisa/player/controllers/httpdto/MonsterEntity.java
@@ -0,0 +1,90 @@
+package imt.fisa.player.controllers.httpdto;
+
+
+public class MonsterEntity {
+    private String id;
+
+    private String name;
+
+    private String element;
+
+    private Integer hp;
+
+    private Integer atk;
+
+    private Integer def;
+
+    private Integer vit;
+
+    private Skill[] skills;
+
+    private Integer level;
+
+    private Integer exp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getAtk() {
+        return atk;
+    }
+
+    public void setAtk(Integer atk) {
+        this.atk = atk;
+    }
+
+    public Integer getDef() {
+        return def;
+    }
+
+    public void setDef(Integer def) {
+        this.def = def;
+    }
+
+    public Integer getVit() {
+        return vit;
+    }
+
+    public void setVit(Integer vit) {
+        this.vit = vit;
+    }
+
+    public Skill[] getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Skill[] skills) {
+        this.skills = skills;
+    }
+}
+
+
